99a0befa58de72eecf3e51bb66aab4c9c2f362ac
Norbert Boros <boros.norbert@freemail.hu>
2018-04-20 22:30:40 +0200

    News activity layout

--- app/src/main/java/com/udacity/norbi930523/manutdapp/ui/NewsRecyclerViewAdapter.java
+++ app/src/main/java/com/udacity/norbi930523/manutdapp/ui/NewsRecyclerViewAdapter.java
@@ -7,8 +7,10 @@ import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.squareup.picasso.Picasso;
 import com.udacity.norbi930523.manutdapp.R;
 import com.udacity.norbi930523.manutdapp.database.news.ArticleColumns;
 
@@ -43,6 +45,15 @@ public class NewsRecyclerViewAdapter extends RecyclerView.Adapter<NewsRecyclerVi
         cursor.moveToPosition(position);
 
         holder.articleTitle.setText(cursor.getString(cursor.getColumnIndex(ArticleColumns.TITLE)));
+        holder.articleDate.setText(cursor.getString(cursor.getColumnIndex(ArticleColumns.DATE)));
+
+        String imageUrl = cursor.getString(cursor.getColumnIndex(ArticleColumns.IMAGE_URL));
+
+        Picasso.with(context)
+                .load(imageUrl)
+                .placeholder(R.drawable.placeholder)
+                .error(R.drawable.placeholder)
+                .into(holder.articleImage);
     }
 
     @Override
@@ -68,8 +79,14 @@ public class NewsRecyclerViewAdapter extends RecyclerView.Adapter<NewsRecyclerVi
 
     class ArticleViewHolder extends RecyclerView.ViewHolder {
 
+        @BindView(R.id.articleImage)
+        ImageView articleImage;
+
         @BindView(R.id.articleTitle)
         TextView articleTitle;
+
+        @BindView(R.id.articleDate)
+        TextView articleDate;
 
         ArticleViewHolder(View itemView) {
             super(itemView);
